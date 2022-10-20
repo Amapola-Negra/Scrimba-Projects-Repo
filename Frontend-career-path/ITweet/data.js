9dcfca2939d5406300b76957970f5c6de4f9af22
Carmen Torralvo <77289768+Amapola-Negra@users.noreply.github.com>
2022-10-20 17:25:10 +0200

    Add files via upload

---
+++ Frontend-career-path/ITweet/data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@Mary GreenFingers <img src="images/ICON-1.png" class="icon">`,
+        profilePic: `images/PERSONAJE-1.png`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Hello!! I am looking for plant lovers to organize a botany seminar. Just let me know. ❤️`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Shelly White <img src="images/ICON-2.png" class="icon">`,
+        profilePic: `images/PERSONAJE-8.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Hello everyone. Is anyone here interested in snow? How about a weekend in the mountains?`,
+        replies: [
+                  {
+                handle: `@Linda <img src="images/ICON-3.png" class="icon">`,
+                profilePic: `images/PERSONAJE-3.png`,
+                tweetText: `Sure! 😎🛩`,
+            },
+                  {
+                handle: `@Peter <img src="images/ICON-2.png" class="icon">`,
+                profilePic: `images/PERSONAJE-4.png`,
+                tweetText: `I'm very excited about it!!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Helen HaveFun <img src="images/ICON-4.png" class="icon">`,
+        profilePic: `images/PERSONAJE-5.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Do you fancy a party? The more of us the better. I need you to confirm attendance as soon as possible. By the way, does anyone own an island?`,
+        replies: [
+            {
+                handle: `@Paul Depressed <img src="images/ICON-5.png" class="icon"`,
+                profilePic: `images/PERSONAJE-6.png`,
+                tweetText: `Pity!! I used to have an island, but I sold it last week. You never know when you're going to need something until you lose it.`,
+            },
+            {
+                handle: `@Paul Depressed <img src="images/ICON-6.png" class="icon"`,
+                profilePic: `images/PERSONAJE-7.png`,
+                tweetText: `I bought an island last week!! I have no idea what it measures. How many will be reading this?`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
---
+++ Frontend-career-path/ITweet/index.js
@@ -0,0 +1,217 @@
+import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+let savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))|| tweetsData
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.thread){
+        handleThreadClick(e.target.dataset.thread)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if(e.target.dataset.addreply){
+        handleMakeReplyClick(e.target.dataset.addreply)
+    }
+    else if(e.target.dataset.close){
+        handleCloseReply(e.target.dataset.close)
+    }
+    else if(e.target.dataset.remove){
+        handleRemoveTweet(e.target.dataset.remove)
+    }
+    else if(e.target.id === 'tweet-btn'){
+        handleTweetBtnClick()
+    }
+})
+function handleLikeClick(tweetId){
+    const targetTweetObj = savedTweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+    savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = savedTweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+    savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+    render() 
+}
+
+function handleThreadClick(replyId){
+    localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+    savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+    /**Here I change the button text depending on the situation 
+     * we have to check previously if we have any reply to show
+    */
+    const targetTweetObj = savedTweetsData.filter(function(tweet){
+        return tweet.uuid === replyId
+    })[0] 
+    if (targetTweetObj.replies.length){
+        if(document.getElementById(`replies-${replyId}`).classList.contains('hidden')){
+            document.getElementById(`thread-${replyId}`).textContent = "Mostrar hilo"
+        }else{
+            document.getElementById(`thread-${replyId}`).textContent = "Ocultar hilo"
+        }
+    }
+}
+function handleReplyClick(replyId){
+    document.getElementById(`reply-items-${replyId}`).classList.remove("hidden")
+}
+function handleMakeReplyClick(replyId){
+    const replyInput = document.getElementById(`textarea-${replyId}`)
+    if(replyInput.value){
+        const replyItem = savedTweetsData.filter(tweet => tweet.uuid === replyId)
+        replyItem[0].replies.push(
+            {
+                handle: `@Myself`,
+                profilePic: `images/LOGO-ITWEET.png`,
+                tweetText: replyInput.value,
+            }
+        )
+        localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+        savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+        render()
+    }
+}
+function handleCloseReply(closeId){
+    document.getElementById(`reply-items-${closeId}`).style.display = "none"
+}
+function handleRemoveTweet(removeTweetId){
+    savedTweetsData =  savedTweetsData.filter(item => item.uuid !== removeTweetId)
+    localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+    savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+    console.log(savedTweetsData)
+    render()
+}
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+    if(tweetInput.value){
+        savedTweetsData.unshift({
+            handle: `@Myself`,
+            profilePic: `images/LOGO-ITWEET.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+        localStorage.setItem("savedTweetsData", JSON.stringify(savedTweetsData))
+        savedTweetsData = JSON.parse(localStorage.getItem("savedTweetsData"))
+        render()
+        tweetInput.value = ''
+    }
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+    savedTweetsData.forEach(function(tweet){
+        let likeIconClass = ''
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        let retweetIconClass = ''
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+        let repliesHtml = ''
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="${reply.profilePic}" class="profile-pic">
+                                <div>
+                                    <p class="handle">${reply.handle}</p>
+                                    <p class="tweet-text">${reply.tweetText}</p>
+                                </div>
+                            </div>
+                    </div>
+                    `
+            })
+        }
+        
+        feedHtml += `
+            <div class="tweet">
+                <div class="tweet-inner">
+                    <img src="${tweet.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${tweet.handle}</p>
+                        <button class="remove" data-remove="${tweet.uuid}">${tweet.handle === "@Myself" ? "Remove" : ""}</button>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                <!-- Here we're going to add new replies-->
+                                <i class="fa-regular fa-comment-dots"
+                                data-reply="${tweet.uuid}"
+                                ></i>
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-heart ${likeIconClass}"
+                                data-like="${tweet.uuid}"
+                                ></i>
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-retweet ${retweetIconClass}"
+                                data-retweet="${tweet.uuid}"
+                                ></i>
+                                ${tweet.retweets}
+                            </span>
+                        </div> 
+                        <!-- This button is going to show all the replies -->
+                        <button class="thread" id="thread-${tweet.uuid}" data-thread="${tweet.uuid}">Mostrar hilo</button>  
+                    </div>            
+                </div>
+                <!-- Form to make a reply-->
+                <div class="hidden reply-form" id="reply-items-${tweet.uuid}" data-replyitems="${tweet.uuid}">
+                    <button class="close" id="close-${tweet.uuid}" data-close="${tweet.uuid}">X</button>
+                    <h4>Reply to ${tweet.handle}</h4>
+                    <textarea id="textarea-${tweet.uuid}" data-textarea="${tweet.uuid}" placeholder="Make your reply"></textarea>
+                    <button id="replyBtn-${tweet.uuid}" data-addreply="${tweet.uuid}">Reply</button>
+                </div>
+                <!-- Here we show the replies-->
+                <div class="hidden" id="replies-${tweet.uuid}">
+                    ${repliesHtml}
+                </div>   
+            </div>
+            `
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
